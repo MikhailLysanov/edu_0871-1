6ba59554651cb34ccd43c5a675c54e4d66fff259
AlexLight <LightWill@users.noreply.github.com>
2020-08-19 15:12:21 +0300

    Update Main.java

--- task_2.4/Main.java
+++ task_2.4/Main.java
@@ -18,7 +18,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        //напишите тут ваш код
         int a = 1;
         for (int i = 2; i < 7; i++) {
             System.out.println(a);
